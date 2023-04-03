4eab86f2560d068fe46280e91ebbb8f8a9125e12
Miguel Silva <silva324@gmail.com>
2023-04-03 22:35:35 +0100

    add example for git private env

--- git/setup.sh
+++ git/setup.sh
@@ -2,4 +2,8 @@
 
 ln -sfn $(pwd)/.git* ~/
 
+# Private info example
+## ln -sfn $(pwd)/example/.git* ~/example/
+
 ln -sfn $(pwd)/myspace/.git* ~/myspace/
+ln -sfn $(pwd)/workspace/.git* ~/workspace/
